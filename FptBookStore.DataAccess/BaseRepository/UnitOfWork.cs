3f131d1e682b0c272b4aaa0957bc94968f5bf8fc
tiendvlp <tiendvlp@gmail.com>
2021-11-02 22:53:39 +0700

    fix conflict

--- FptBookStore.DataAccess/BaseRepository/UnitOfWork.cs
+++ FptBookStore.DataAccess/BaseRepository/UnitOfWork.cs
@@ -1,13 +1,10 @@
 using FptBookStore.DataAccess.BaseRepository.Interface;
-using FptBookStore.DataAccess.Company;
 using FptBookStore.DataAccess.Data;
 using FptBookStore.DataAccess.IdentityUserRole;
 using FptBookStore.DataAccess.OrderDetails;
 using FptBookStore.DataAccess.Orders;
 using FptBookStore.DataAccess.Products;
 using FptBookStore.DataAccess.Repository;
-using FptBookStore.DataAccess.ShoppingCarts;
-using FptBookStore.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,11 +18,9 @@ namespace FptBookStore.DataAccess.Categories
         private readonly ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
-        public ICompanyRepository Company { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IIdentityRoleRepository IdentityRole { get; private set; }
         public IIdentityUserRoleRepository IdentityUserRole { get; private set; }
-        public IShoppingCartRepository ShoppingCart { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public ISP_Call SP_Call { get; private set; }
@@ -39,11 +34,9 @@ namespace FptBookStore.DataAccess.Categories
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
-            Company = new CompanyRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
             IdentityRole = new IdentityRoleRepository(_db);
             IdentityUserRole = new IdentityUserRoleRepository(_db);
-            ShoppingCart = new ShoppingCartRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
             SP_Call = new SP_Call(_db);
