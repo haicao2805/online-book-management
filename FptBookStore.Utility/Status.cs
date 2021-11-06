28661415e56ad5ac1419a72a43c64f9274ddfd4f
ngocthieuff <ptnngoc22@gmail.com>
2021-11-06 20:50:57 +0700

    clean reset password

--- FptBookStore.Utility/Status.cs
+++ FptBookStore.Utility/Status.cs
@@ -8,9 +8,12 @@ namespace FptBookStore.Utility
 {
     public class Status
     {
+        public const string Pending = "Pending";
         public const string Approved = "Approved";
         public const string Processing = "Processing";
         public const string Shipped = "Shipped";
+        public const string Canceled = "Canceled";
         public const string Refunded = "Refunded";
+        public const string Rejected = "Rejected";
     }
 }
